9dbfcefbe2a41127c6ada045dd1b3f226c722cf0
James Sinkala <xinnks@gmail.com>
2022-05-01 22:01:13 +0300

    feat(index.js): Add service subscription route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Router } from 'itty-router';
 import { rawHtmlResponse } from './utils';
 import { indexHtml } from './fns/html';
+import { Subscribe } from './fns';
+import { indexHtml, messageHtml, successHtml } from './html';
 // Create a new router
 const router = Router();
 
@@ -12,6 +14,28 @@ router.get("/", () => {
   return rawHtmlResponse(indexHtml);
 })
 
+/** This route subscribes a user to the my-daily-reads service
+ * @param {Request} {params}
+ * @returns {Response}
+*/
+router.post("/subscribe", async request => {
+  let response = {}, email = "", name = "", keywords = "";
+  let reqBody = await readRequestBody(request);
+  
+  ({name, email, keywords} = reqBody);
+
+  if (!name || !email || !keywords) {
+    return rawHtmlResponse(messageHtml("Missing fields", `${!name?'name, ':''}${!email?'email, ':''}${!keywords?'keywords, ':''} are required.`))
+  }
+
+  // filter keywords  
+  let filteredKeywords = keywords.match(/^([\w]+[ ]*[,]*[ ]*[\w]+)/gi);
+  let screenedKeywordsData = filteredKeywords.length ? filteredKeywords.join(",") : filteredKeywords.join("");
+  
+  let {status, message} = await Subscribe({name, email, keywords: screenedKeywordsData});
+  console.error("{status, message}: -- ", {status, message});
+  return status === "success" ? rawHtmlResponse(successHtml) : rawHtmlResponse(messageHtml("Failed to subscribe", message));
+})
 addEventListener('fetch', (e) => {
   e.respondWith(router.handle(e.request));
 })
