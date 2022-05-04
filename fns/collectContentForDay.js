0738aa836109e6a7fef86f288aa3b3e978ce0932
James Sinkala <xinnks@gmail.com>
2022-05-04 06:41:28 +0300

    feat: Update daily-content-collection function

--- fns/collectContentForDay.js
+++ fns/collectContentForDay.js
@@ -1,35 +1,39 @@
-const { fetchDevToArticlesFromAPI } = require("./../content");
-const { formatPostsDataSchema } = require("./../content/posts");
+const { fetchDevToArticlesFromAPI } = require("./../content/devto_content");
+const { formatPostsDataSchema } = require("./../content/posts/formatPostsDataSchema");
 const { formatDate, dateDifference } = require("./../content/helpers/utils");
-const { deleteDocuments, insertDocuments, fetchAllCollectionData } = require("./../db");
+const { deleteDocuments, insertDocuments, fetchAllCollectionData, countCollectionData } = require("./../db");
 const todayDate = formatDate(new Date(), "dashedDate");
 const yesterdayDate = formatDate(dateDifference(new Date(), -1), "dashedDate");
+const { fetchHashnodePostsFromAPI } = require('./../content/hashnode_content');
 
 /**
  * @description This function fetches and compiles the latest content from sources, formats to a preffered schema and stores them into the database
 **/
 export async function CollectContentForDay(contentLimit, previousInsertDate = yesterdayDate){
-  let message, formattedDevToContent;
-  let {state, body: devToContent} = await fetchDevToArticlesFromAPI(1, contentLimit);
+  let message, formattedDevToContent, formattedHashnodeContent;
+  let {state, body: hashnodeContent} = await fetchHashnodePostsFromAPI();
   if(state === "success"){
-    formattedDevToContent = await formatPostsDataSchema(devToContent, 0, devToContent.length, 'dev.to');
+    formattedHashnodeContent = await formatPostsDataSchema(hashnodeContent, 0, hashnodeContent.length, 'hashnode');
   } else {
     return {status: "failure", message: "Failed to fetch DevTo API data"}
   }
   
   // collect new day content
-  const allContentWithInsertDate = formattedDevToContent.map(post => ({...post, insertDate: todayDate}));
+  const allContentWithInsertDate = formattedHashnodeContent.map(post => ({...post, insertDate: todayDate}));
   
   // check if previous day content exists ( check if content stash collection is not empty )
-  let {existingContentStashState, body: existingContentStash} = await fetchAllCollectionData(ALL_CONTENT_INDEX);
-  console.log("existing documents: --- ", existingContentStash.length);
-  if(existingContentStashState === "success" && existingContentStash.length){
-    // delete previous day content
-    let {state: deletionStatus} = await deleteDocuments(existingContentStash.map(x => x.refId), CONTENT_COLLECTION);
-    console.log("deleted documents: --- ", deletionStatus);
-    if(deletionStatus === "failure"){
-      message = "Could not delete documents";
-      // TODO: send error log email
+  let {state: existingContentCountState, body: existingContentCount} = await countCollectionData(ALL_CONTENT_INDEX);
+  if(existingContentCountState === "success" && existingContentCount){
+    let {state: fetchState, body: existingContentStash} = await fetchAllCollectionData(ALL_CONTENT_INDEX);
+    if(fetchState === "success"){
+      // delete previous day content
+      let {state: deletionStatus} = await deleteDocuments(existingContentStash.map(x => x.refId), CONTENT_COLLECTION);
+      if(deletionStatus === "failure"){
+        message = "Could not delete documents";
+        // TODO: send error log email
+      }
+    } else {
+      message = "Could not fetch docs to delete";
     }
   }
   
