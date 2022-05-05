f2fe27da66c79cd8f35b8396e621bd525d3f4369
James Sinkala <xinnks@gmail.com>
2022-05-05 22:39:00 +0300

    feat(html/index.js): Add KeywordsUpdateHtml and UpdateKeywordsRequestHtml markups locations to barrel file

--- html/index.js
+++ html/index.js
@@ -1,8 +1,10 @@
 const { indexHtml } = require("./indexHtml");
 const { messageHtml } = require("./messageHtml");
 const { successHtml } = require("./successHtml");
+const { KeywordsUpdateHtml } = require("./keywordsUpdateHtml");
 const { NotFoundHtml } = require("./notFoundHtml");
 const { otpEmailHtml } = require("./otpEmailHtml");
+const { UpdateKeywordsRequestHtml } = require("./updateKeywordsRequestHtml");
 const { ContentEmailHtml } = require("./contentEmailHtml");
 const { WelcomeEmailHtml } = require("./welcomeEmailHtml");
 const { FarewellEmailHtml } = require("./farewellEmailHtml");
@@ -12,8 +14,10 @@ module.exports = {
   indexHtml,
   messageHtml,
   successHtml,
+  KeywordsUpdateHtml,
   NotFoundHtml,
   otpEmailHtml,
+  UpdateKeywordsRequestHtml,
   FarewellEmailHtml,
   ContentEmailHtml,
   WelcomeEmailHtml,
