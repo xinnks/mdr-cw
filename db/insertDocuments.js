eedca1693caf46fe2df5d5265d7eba866fc4a393
James Sinkala <xinnks@gmail.com>
2022-05-05 04:58:27 +0300

    improvement(db/insertDocuments.js): Update function

--- db/insertDocuments.js
+++ db/insertDocuments.js
@@ -1,4 +1,8 @@
-const {Map, Lambda, Create, Collection, Var, faunaClient, getFaunaError} = require("./client");
+const faunadb = require('faunadb');
+const faunaClient = new faunadb.Client({
+  secret: FAUNA_SECRET
+})
+const { Map, Lambda, Create, Collection, Var } = faunadb.query;
 
 /**
  * @description This function inserts multiple documents into a collection
@@ -6,27 +10,20 @@ const {Map, Lambda, Create, Collection, Var, faunaClient, getFaunaError} = requi
  * @param { String } collection => Collection name
 **/
 export async function insertDocuments(docs, collection){
-  try {
-    const result = await faunaClient.query(
-      Map(
-        docs,
-        Lambda("doc",
-          Create(
-            Collection(collection),
-            {
-              data: Var("doc")
-            }
-          )))
-    );
-    
-    return {
-      state: "success",
-      body: result.data
-    }
-  } catch (error) {
-    return {
-      state: "error",
-      body: getFaunaError(error)
-    };
+  const result = await faunaClient.query(
+    Map(
+      docs,
+      Lambda("doc",
+        Create(
+          Collection(collection),
+          {
+            data: Var("doc")
+          }
+        )))
+  );
+  
+  return {
+    state: "success",
+    body: result.data
   }
 }
