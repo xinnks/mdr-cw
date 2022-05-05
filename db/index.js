fc3861b1e92ebd11e1541aea1ebc2b037133f5cd
James Sinkala <xinnks@gmail.com>
2022-05-06 00:55:40 +0300

    feat(db/index.js): Add deleteDocument and insertDocument function locations to database barrel file

--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
+const { insertDocument } = require( "./insertDocument");
 const { insertDocuments } = require( "./insertDocuments");
+const { deleteDocument } = require( "./deleteDocument");
 const { deleteDocuments } = require( "./deleteDocuments");
 const { updateDocument } = require( "./updateDocument");
 const { findDocument } = require( "./findDocument");
@@ -6,8 +8,10 @@ const { fetchAllCollectionData } = require( "./fetchAllCollectionData");
 const { countCollectionData } = require( "./countCollectionData");
 
 module.exports = {
+  insertDocument,
   insertDocuments,
   findDocument,
+  deleteDocument,
   deleteDocuments,
   updateDocument,
   fetchAllCollectionData,
