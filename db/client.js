07ae8f67068c07925e2b80ee265c9001952a2156
James Sinkala <xinnks@gmail.com>
2022-05-05 05:11:01 +0300

    feat(db/client.js): Add more database query functions

--- db/client.js
+++ db/client.js
@@ -3,8 +3,8 @@ const { getFaunaError } = require('./db-utils.js');
 const faunaClient = new faunadb.Client({
   secret: FAUNA_SECRET
 })
-const {Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Update} = faunadb.query;
+const {Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Count, Update} = faunadb.query;
 
 module.exports = {
-  Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Update, faunaClient, getFaunaError
+  Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Count, Update, faunaClient, getFaunaError
 }
