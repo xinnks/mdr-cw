7ff707e948f08f85f489aa6fa80eb3df37a9e496
James Sinkala <xinnks@gmail.com>
2022-05-05 20:34:10 +0300

    feat(html/index.js): Add imports to barrel file

--- html/index.js
+++ html/index.js
@@ -2,13 +2,18 @@ const { indexHtml } = require("./indexHtml");
 const { messageHtml } = require("./messageHtml");
 const { successHtml } = require("./successHtml");
 const { NotFoundHtml } = require("./notFoundHtml");
+const { otpEmailHtml } = require("./otpEmailHtml");
 const { ContentEmailHtml } = require("./contentEmailHtml");
 const { WelcomeEmailHtml } = require("./welcomeEmailHtml");
+const { UnsubscribeRequestHtml } = require("./unsubscribeRequestHtml");
+
 module.exports = {
   indexHtml,
   messageHtml,
   successHtml,
   NotFoundHtml,
+  otpEmailHtml,
   ContentEmailHtml,
   WelcomeEmailHtml,
+  UnsubscribeRequestHtml
 }
