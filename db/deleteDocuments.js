70da81e78787eebc7bc00307095fcf79190eb17f
James Sinkala <xinnks@gmail.com>
2022-05-07 05:01:19 +0300

    improvement: Catch errors in function

--- db/deleteDocuments.js
+++ db/deleteDocuments.js
@@ -3,29 +3,36 @@ const { getFaunaError } = require('./db-utils.js');
 const faunaClient = new faunadb.Client({
   secret: FAUNA_SECRET
 })
-const { Map, Lambda, Delete, Ref, Collection, Var } = faunadb.query;
+const { Collection, Map, Ref, Lambda, Delete, Select, Var } = faunadb.query;
 
 /**
  * @description This function deletes many documents
  * @param { Array } refId => An Array of documents' ref ids
- * @param { String } collectionIndex => Collection index
+ * @param { String } collectionName => Collection name
 **/
-export async function deleteDocuments(refIds, collectionIndex){
-  const result = await faunaClient.query(
-    Map(
-      refIds,
-      Lambda("refId",
-        Delete(
-          Ref(
-            Collection(collectionIndex), Var("refId")
+export async function deleteDocuments(refIds, collectionName){
+  try {
+    const result = await faunaClient.query(
+      Map(
+        refIds,
+        Lambda("refId",
+          Delete(
+            Ref(
+              Collection(collectionName), Var("refId")
+            )
           )
         )
       )
-    )
-  );
-  
-  return {
-    state: "success",
-    body: result.data
+    );
+    
+    return {
+      state: "success",
+      body: result
+    }
+  } catch (error) {
+    return {
+      state: "error",
+      body: getFaunaError(error)
+    };
   }
 }
